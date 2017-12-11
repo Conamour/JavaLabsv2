24daed3c1caf3e844b1b7119a9f2000b45cf7a11
JJ <mr.freedom77@gmail.com>
2017-12-12 02:18:04 +0500

    add Multithread
    //

---
+++ Ex9Dinner/src/Ex9Dinner.java
@@ -0,0 +1,20 @@
+public class Ex9Dinner {
+
+    public static void main(String[] args) throws InterruptedException {
+        Fork[] forks = new Fork[5];
+        for (int i = 0; i < forks.length; i++) {
+            forks[i] = new Fork(i);
+        }
+
+        Philosopher[] philosophers = new Philosopher[5];
+        int j;
+        for (j = 0; j < philosophers.length-1; j++) {
+            philosophers[j] = new Philosopher(j, forks[j], forks[(j + 1) % forks.length]);
+        }
+
+        philosophers[j] = new Philosopher(j, forks[0], forks[j]);
+        for (int i = 0; i < philosophers.length; i++) {
+            new Thread(philosophers[i]).start();
+        }
+    }
+}
